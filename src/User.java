97b2747eb8ea4801dc09994f9220a7b5b1e006fc
Alexandre Gravereaux <alexandre.gravereaux@student-cs.fr>
2025-05-16 16:44:26 +0200

    [ADD] User class

--- src/User.java
+++ src/User.java
@@ -1,2 +1,27 @@
 public class User {
+    private static int id;
+    private String username;
+    private String password;
+
+    public User(String username, String password) {
+        id++;
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
